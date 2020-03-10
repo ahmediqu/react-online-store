9170b0e7cee4787b20a08601095e95a704043932
Iqbal ahmed tuhin <jniqbal1@gmail.com>
2020-03-10 16:02:50 +0600

    Online Store

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React , { Fragment } from 'react';
+import { BrowserRouter as Router , Route , Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import $ from "jquery";
+import { Manager, Reference, Popper } from 'react-popper';
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Product from './components/Product';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Default from './components/Default';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => (
+ <Router>
+    <Fragment>
+      <Navbar />
+      {/* <Cart />
+      <Product />
+      <ProductList />
+      <Details />
+      <Default /> */}
+        <Route path="/" exact component={ ProductList }/>
+
+      <section className="container">
+        <Switch>
+          <Route exact path="/product" component={ Product } />
+          <Route exact path="/details" component={ Details } />
+          <Route exact path="/cart" component={ Cart } />
+          <Route exact  component={ Default } />
+        </Switch>
+      </section>
+      
+    </Fragment>
+  </Router>
+);
 
 export default App;
---
+++ src/components/Default.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+export default class Default extends Component {
+    render(){
+        return (
+            <div>Default</div>
+        );
+    }
+}
